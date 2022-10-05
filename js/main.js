14fba83070a985def7f3093090fc16f61a0be3f7
matheus gabriel <matheusgabriel@brasilcard.net>
2022-10-05 16:49:13 -0300

    botao de exluir os elementos

--- js/main.js
+++ js/main.js
@@ -22,8 +22,9 @@ form.addEventListener("submit", (evento) => {
     if(exitse){
         itemAtual.id = exitse.id
         atualizaEmento(itemAtual)
+        itens[itens.findIndex(elemento => elemento.id === exitse.id)] = itemAtual
     }else {
-        itemAtual.id = itens.length
+        itemAtual.id = itens[itens.length -1] ? (itens[itens.length-1]).id + 1: 0
         criaElemento(itemAtual)
         itens.push(itemAtual)
     }
@@ -45,11 +46,27 @@ function criaElemento(item){
     novoItem.appendChild(numeroItem)
 
     novoItem.innerHTML += item.nome
-
+    novoItem.appendChild(botaoDeleta(item.id))
     lista.appendChild(novoItem)
    
 }
 
 function atualizaEmento(item){
-    document.querySelector("[data-id"+item.id+"")
+    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade
+}
+
+function botaoDeleta(id){
+    const elementoBotao = document.createElement("button")
+    elementoBotao.innerText = "X"
+    elementoBotao.addEventListener("click", function(){
+        deletaElemento(this.parentNode, id)
+    })
+
+    return elementoBotao
+}
+
+function deletaElemento(tag){
+    tag.remove()
+
+    itens.splice(itens.findIndex(elemento => elemento.id === id))
 }
